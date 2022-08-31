68626d35f41d6a7c8c5a78a8e34f957da55e3920
Dhani Somaskandan <dhani.somaskandan@elev.ga.ntig.se>
2022-08-31 14:23:16 +0200

    Commit 1

---
+++ Lucktext/Lucktext/Program.cs
@@ -0,0 +1,32 @@
+﻿// Console.WriteLine("Hej NTI"); 
+// Console.WriteLine("Hej TE21B"); 
+
+
+string name;
+string mat; 
+string sport;
+string ovän;
+
+// name = "Dhani";
+// name = "Alex";
+
+Console.WriteLine ("Vad heter du?");
+name = Console.ReadLine();
+
+// Console.WriteLine($"Hello  {name}!");
+
+
+Console.WriteLine ("Vad är din favorit dryck?");
+mat = Console.ReadLine();
+
+Console.WriteLine ("Vad är din favorit sport");
+sport = Console.ReadLine ();
+
+Console.WriteLine ("Vem hatar du?");
+ovän = Console.ReadLine();
+
+Console.WriteLine ($"Hejsan svejsan {name}, du har blivit inbjuden till att spela {sport} för ditt favorit lag! Tyvärr so har vi dock slut på {mat} men vi har varmt piss redo att dricka! Din rumskamrat är {ovän} som du kommer bo med i 3 år från och med nu. Hoppas att du trivs {name}.");
+
+Console.ReadLine ();
+
+
